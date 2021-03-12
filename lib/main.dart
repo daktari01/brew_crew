a66047072d0604accdbfd6e2dc64c780ee7f38ca
James Dindi <dindijjames@gmail.com>
2021-03-12 13:26:13 +0300

    Using the Provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:brew_crew/models/fb_user.dart';
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_crew/screens/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,8 +15,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<FbUser>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,12 +1,17 @@
+import 'package:brew_crew/models/fb_user.dart';
 import 'package:flutter/material.dart';
 import 'package:brew_crew/screens/home/home.dart';
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    
+    final user = Provider.of<FbUser>(context);
+    print(user);
 
     // Return either Home or Authenticate widget
     return Container(
