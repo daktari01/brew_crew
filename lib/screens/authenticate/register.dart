83e316fd949cca733ddd51d6e75bfb86c886cfad
James Dindi <dindijjames@gmail.com>
2021-03-12 17:35:32 +0300

    Register with email and password

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -13,10 +13,12 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
 
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // Text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -43,41 +45,57 @@ class _RegisterState extends State<Register> {
       body: Container(
           padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
           child: Form(
-              child: Column(
-                children: [
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    onChanged: (val) {
-                      setState(() {
-                        email = val;
-                      });
-                    },
+            key: _formKey,
+            child: Column(
+              children: [
+                SizedBox(height: 20.0),
+                TextFormField(
+                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                  onChanged: (val) {
+                    setState(() {
+                      email = val;
+                    });
+                  },
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                  validator: (val) => val.length < 6 ? 'Password must be at least 6 chars long' : null,
+                  obscureText: true,
+                  onChanged: (val) {
+                    setState(() {
+                      password = val;
+                    });
+                  },
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      dynamic result = await _auth.registerWithEmailAndPassword(email, password);
+                      if (result == null) {
+                        setState(() {
+                          error = 'Please supply a valid email';
+                        });
+                      }
+                    }
+                  },
+                  child: Text(
+                      'Register'
                   ),
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                    obscureText: true,
-                    onChanged: (val) {
-                      setState(() {
-                        password = val;
-                      });
-                    },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.pink[400],
                   ),
-                  SizedBox(height: 20.0),
-                  ElevatedButton(
-                    onPressed: () async {
-                      print('********************');
-                      print('Email: $email and Password: $password');
-                      print('********************');
-                    },
-                    child: Text(
-                        'Register'
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.pink[400],
-                    ),
-                  )
-                ],
-              )
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  error,
+                  style: TextStyle(
+                    color: Colors.redAccent,
+                    fontSize: 14.0,
+                  ),
+                )
+              ],
+            )
           )
       ),
     );
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -30,6 +30,17 @@ class AuthService {
   // Sign in with email and password
 
   // Register with email and password
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   // Sign out
   Future signOut() async {
